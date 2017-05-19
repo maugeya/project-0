9c2418e49f7af9800d6fa970ba6d84c13d7ded90
Angela Maugey <a.maugey@gmail.com>
2017-05-19 16:47:01 +0100

    logic started

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,37 @@
-console.log('JS Loaded');
+$(() => {
+  console.log('JS Loaded');
+
+  const $takeTile = $('.takeTile');
+  const $tiles = $('.tiles');
+
+  let score = 10;
+
+  console.log($tiles);
+
+  $takeTile.on('click', () => {
+
+    $tiles.hide('slow', 'swing', () => {
+      
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
