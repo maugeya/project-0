c69bc5d5b281b7de52e3bd89097f3fd8b7bf414f
Angela Maugey <a.maugey@gmail.com>
2017-05-21 14:34:18 +0100

    Refactoring the grid functions

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,12 @@ $(() => {
   const $unhidden = $('.unhidden');
   const $playerScore = $('.playerScore');
 
-  let score = 10;
-
+  let score = Math.abs(10);
   const $settingPlayerScore = $playerScore.html(score);
 
-  console.log($tiles);
+  //logic to take away a random tile when button pressed
 
-  $takeTile.on('click', () => {
-
+  function takeTile () {
     const $randomNum = Math.floor(Math.random() * 9);
 
     const $arrayOfTiles = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
@@ -24,32 +22,76 @@ $(() => {
     // const $usedTiles = [];
     // $usedTiles.push($randomTileSelected);
     // console.log($usedTiles);
-    const changeClass = $tiles.eq($randomTileSelected).removeClass('unhidden').addClass('hidden');
+    const $changeClass = $tiles.eq($randomTileSelected).removeClass('unhidden').addClass('hidden');
 
 
-    const checkingClasses = function checkingClasses() {
-      if ((changeClass.hasClass('hidden')) === true) {
-        return $randomTileSelected;
-      } else {
-        return changeClass;
-      }
-    };
+  }
 
+  function updateScore() {
 
+    score -= 1;
+    $playerScore.html(Math.abs(score));
+
+  }
+
+  // // const checkingClasses = function checkingClasses() {
+  // //   if (($changeClass.hasClass('hidden')) === true) {
+  // //     return $randomTileSelected;
+  // //   } else {
+  // //     return $changeClass;
+  // //   }
+  // };
 
     // });
 
-  });
+  // });
 
-  //Function to return the player input
+
+
+  //setting const for checking form on submission
+
   const $playerAnswer = $('#playerAnswer');
-  $('form').on('submit', function(event) {
+
+  const $objectOfCorrectAnswers =
+    {
+      roundOne: 'alaska thunderfuck 5000',
+      roundTwo: 'alyssa edwards',
+      roundThree: 'bianca del rio',
+      roundFour: 'bob the drag queen',
+      roundFive: 'willam belli',
+      roundSix: 'katya zamolodchikova'
+    };
+
+  const $gridClass = ('.roundOne');
+  const $form = $('form');
+
+
+//setting up the form to not refresh page on submit
+//getting answers from player submitting
+//clearing form after submitting
+
+  $form.on('submit', function(event) {
     event.preventDefault();
-      console.log($playerAnswer.val());
 
     const $stringAnswer = $playerAnswer.val();
+    console.log($stringAnswer);
+
+    const $lowerCaseStringAnswer = $stringAnswer.toLowerCase();
+    console.log($lowerCaseStringAnswer);
+
     $('#playerInputForm').children('input').val('');
-    return $stringAnswer;
+
+
+    //function to check answer...
+
+    function checkAnswers() {
+
+    }
+
+
+
+
+    // checkAnswer();
 
   });
 
@@ -57,6 +99,18 @@ $(() => {
 
 
 
+  console.log($objectOfCorrectAnswers);
+
+
+
+
+// // const checkAnswer = function checkAnswer() {
+// //
+// //    if (($lowerCaseStringAnswer === objectOfCorrectAnswers) && ($gridClass === objectOfCorrectAnswers[key])) {
+// //
+// //    }
+//
+// };
 
 
 
@@ -64,6 +118,12 @@ $(() => {
 
 
 
+
+
+
+
+  $takeTile.on('click', takeTile);
+  $takeTile.on('click', updateScore);
 
 
 
