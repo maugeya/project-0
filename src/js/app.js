cf07a8d8e26a3d4ab8fca251d759c269111b2d8b
Angela Maugey <a.maugey@gmail.com>
2017-05-21 12:06:45 +0100

    logic to get player input from form

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,11 @@ $(() => {
   const $takeTile = $('.takeTile');
   const $tiles = $('.tiles');
   const $unhidden = $('.unhidden');
+  const $playerScore = $('.playerScore');
 
   let score = 10;
+
+  const $settingPlayerScore = $playerScore.html(score);
 
   console.log($tiles);
 
@@ -15,19 +18,40 @@ $(() => {
 
     const $arrayOfTiles = ['0', '1', '2', '3', '4', '5', '6', '7', '8'];
 
-    let $randomTileSelected = $arrayOfTiles.splice([$randomNum], 1);
+    const $randomTileSelected = $arrayOfTiles.splice([$randomNum], 1);
     console.log($randomTileSelected);
 
     // const $usedTiles = [];
     // $usedTiles.push($randomTileSelected);
     // console.log($usedTiles);
+    const changeClass = $tiles.eq($randomTileSelected).removeClass('unhidden').addClass('hidden');
 
-       $tiles.eq($randomTileSelected).removeClass('unhidden').addClass('hidden');
+
+    const checkingClasses = function checkingClasses() {
+      if ((changeClass.hasClass('hidden')) === true) {
+        return $randomTileSelected;
+      } else {
+        return changeClass;
+      }
+    };
+
+
 
     // });
 
   });
 
+  //Function to return the player input
+  const $playerAnswer = $('#playerAnswer');
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+      console.log($playerAnswer.val());
+
+    const $stringAnswer = $playerAnswer.val();
+    $('#playerInputForm').children('input').val('');
+    return $stringAnswer;
+
+  });
 
 
 
