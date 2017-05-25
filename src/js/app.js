ab7e676bd6afffa077f4d4b4545ffc48c35143d0
Angela Maugey <a.maugey@gmail.com>
2017-05-25 16:46:54 +0100

    code with namespacing too

--- src/js/app.js
+++ src/js/app.js
@@ -1,238 +1,233 @@
-$(() => {
-  console.log('JS Loaded');
-
-  const $introAudio = $('.introButtonAudio')[0];
-  const $resultAudio = $('.resultAudio')[0];
-  const $takeTileAudio = $('.takeTileAudio')[0];
-  const $completeAudio = $('.completeAudio')[0];
-  const $takeTile = $('.takeTile');
-  const $tiles = $('.tiles');
-  const $playerScore = $('.playerScore');
-  const $result = $('.result');
-  const $gifs = $('#gifs');
-  const $reset = $('.reset');
-  const $grid = $('#grid');
-  const $submitBtn = $('.submitAnswer');
-  const $complete = $('.complete');
-  const $bonusRound = $('.bonusRound');
-  const $body = $('body');
-  const $afterRoundsScore = $('.afterRoundsScore');
-  const $nextRoundBtn = $('.nextRoundBtn');
-  const $playerAnswer = $('#playerAnswer');
-  const $form = $('form');
-
-  const $bonusAnswerQueen = $('#nameQueen');
-  const $bonusAnswerSeason = $('#numberSeason');
-  const $submitBonusAnswer = $('.submitBonusAnswer');
-  const $finalScore = $('.finalScore');
-  const $show = $('.show');
-  const $logo = $('.logo');
-  const $footer = $('footer');
-  const $main = $('main');
-
-  const arrayOfCorrectAnswers =
-    [
-      'alaska thunderfuck 5000',
-      'alyssa edwards',
-      'bianca del rio',
-      'bob the drag queen',
-      'willam belli',
-      'katya zamolodchikova'
-    ];
-
-  let arrayOfTiles = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-  const arrayOfRounds = [
-    'roundOne',
-    'roundTwo',
-    'roundThree',
-    'roundFour',
-    'roundFive',
-    'roundSix',
-    'bonusRound'];
-
-  let level = 0;
-  let score = 10;
-  let i = 0;
-
-  $logo.hide();
-  $footer.hide();
-
-  $playerScore.html(score);
-  $grid.addClass(arrayOfRounds[level]);
-  $nextRoundBtn.hide();
+var rpdg = rpdg || {};
+console.log('JS Loaded');
 
 
 
-  function hideIntroduction() {
-    $main.show();
-    $show.slideToggle('slow');
-    $logo.show();
-    $footer.show();
+rpdg.arrayOfCorrectAnswers = [
+  'alaska thunderfuck 5000',
+  'alyssa edwards',
+  'bianca del rio',
+  'bob the drag queen',
+  'willam belli',
+  'katya zamolodchikova'
+];
+
+rpdg.arrayOfTiles = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+
+rpdg.arrayOfRounds = [
+  'roundOne',
+  'roundTwo',
+  'roundThree',
+  'roundFour',
+  'roundFive',
+  'roundSix',
+  'bonusRound'
+];
+
+rpdg.level = 0;
+rpdg.score = 10;
+rpdg.i = 0;
+
+
+
+
+rpdg.hideIntroduction = function hideIntroduction() {
+  this.$main.show();
+  this.$show.slideToggle('slow');
+  this.$logo.show();
+  this.$footer.show();
+};
+
+
+
+rpdg.soundClipIntro = function soundClipIntro() {
+  this.$introAudio.src = ('/public/assets/audio/intro-car.wav');
+  this.$introAudio.play();
+
+};
+
+
+rpdg.takeTile = function takeTile() {
+  const randomNum = Math.floor(Math.random() * this.arrayOfTiles.length);
+  const randomTileIndex = this.arrayOfTiles.splice(randomNum, 1);
+  this.$tiles.eq(randomTileIndex).addClass('hidden');
+  this.$takeTileAudio.src = ('/public/assets/audio/tile-remove.mp3');
+  this.$takeTileAudio.play();
+
+};
+
+rpdg.updateScore = function updateScore() {
+
+  if (this.score === 0) {
+    this.$result.html('Better luck next time, press reset to try again!');
+    // $result.addClass('lose');
+    this.$gifs.attr('src', '/public/assets/images/lose.gif');
+
+  } else {
+    this.score -= 1;
+    this.$playerScore.html(Math.abs(this.score));
   }
 
+};
 
 
-  function soundClipIntro() {
-    $introAudio.src = ('/public/assets/audio/intro-car.wav');
-    $introAudio.play();
+rpdg.getInput = function getInput() {
+  event.preventDefault();
 
-  }
+  const $stringAnswer = this.$playerAnswer.val();
+  console.log($stringAnswer);
 
+  const $lowerCaseStringAnswer = $stringAnswer.toLowerCase();
+  console.log($lowerCaseStringAnswer);
 
-  function takeTile() {
-    const randomNum = Math.floor(Math.random() * arrayOfTiles.length);
-    const randomTileIndex = arrayOfTiles.splice(randomNum, 1);
-    $tiles.eq(randomTileIndex).addClass('hidden');
-    $takeTileAudio.src = ('/public/assets/audio/tile-remove.mp3');
-    $takeTileAudio.play();
+  if (this.$grid.hasClass('roundSix') && ($lowerCaseStringAnswer === this.arrayOfCorrectAnswers[this.i])) {
 
-  }
+    this.$bonusRound.slideToggle('slow');
+    this.$afterRoundsScore.html(this.score);
+    this.$body.addClass('frozen');
+    this.$resultAudio.src = '/public/assets/audio/before-bonus.mp3';
 
-  function updateScore() {
+  } else if ($lowerCaseStringAnswer === this.arrayOfCorrectAnswers[this.i]){
+    this.$result.html('You clocked it! Press the Next Round button to take it to the next level!');
+    this.level ++;
+    this.$submitBtn.hide();
+    console.log('Next level to play =', this.arrayOfRounds[this.level]);
+    this.$gifs.attr('src', '/public/assets/images/win.gif');
+    this.$resultAudio.src = ('/public/assets/audio/celebration.mp3');
+    this.$resultAudio.play();
+    this.$nextRoundBtn.show();
+    this.$takeTile.hide();
 
-    if (score === 0) {
-      $result.html('Better luck next time, press reset to try again!');
-      // $result.addClass('lose');
-      $gifs.attr('src', '/public/assets/images/lose.gif');
-
-    } else {
-      score -= 1;
-      $playerScore.html(Math.abs(score));
-    }
-
-  }
-
-
-  function getInput() {
-    event.preventDefault();
-
-    const $stringAnswer = $playerAnswer.val();
-    console.log($stringAnswer);
-
-    const $lowerCaseStringAnswer = $stringAnswer.toLowerCase();
-    console.log($lowerCaseStringAnswer);
-
-    if ($grid.hasClass('roundSix') && ($lowerCaseStringAnswer === arrayOfCorrectAnswers[i])) {
-
-      $bonusRound.slideToggle('slow');
-      $afterRoundsScore.html(score);
-      $body.addClass('frozen');
-      $resultAudio.src = '/public/assets/audio/before-bonus.mp3';
-
-    } else if ($lowerCaseStringAnswer === arrayOfCorrectAnswers[i]){
-      $result.html('You clocked it! Press the Next Round button to take it to the next level!');
-      level ++;
-      $submitBtn.hide();
-      console.log('Next level to play =', arrayOfRounds[level]);
-      $gifs.attr('src', '/public/assets/images/win.gif');
-      $resultAudio.src = ('/public/assets/audio/celebration.mp3');
-      $resultAudio.play();
-      $nextRoundBtn.show();
-      $takeTile.hide();
-
-    } else {
-      $result.html('Why dontcha try another square Henny?');
-      $gifs.attr('src', '/public/assets/images/try-another.gif');
-      $resultAudio.src = ('/public/assets/audio/try-another.mp3');
-
-    }
+  } else {
+    this.$result.html('Why dontcha try another square Henny?');
+    this.$gifs.attr('src', '/public/assets/images/try-another.gif');
+    this.$resultAudio.src = ('/public/assets/audio/try-another.mp3');
 
   }
 
-  function clearInput() {
-    $playerAnswer.val('');
+};
+
+rpdg.clearInput = function clearInput() {
+  this.$playerAnswer.val('');
+};
+
+rpdg.clearingClassesAndDivs = function clearingClassesAndDivs() {
+  this.$takeTile.show();
+  this.$tiles.removeClass('hidden');
+  this.$result.html('');
+  this.$gifs.attr('src', '/public/assets/images/blank.gif');
+  this.arrayOfTiles = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  this.$playerScore.html(this.score);
+};
+
+rpdg.nextRound = function nextRound() {
+  this.$submitBtn.show();
+  this.score += 10;// add ten for the next level
+  this.i++;
+  this.$grid.addClass(this.arrayOfRounds[this.level]);
+  this.$nextRoundBtn.hide();//hide the next round button
+  rpdg.clearingClassesAndDivs();
+};
+
+rpdg.resetGame = function resetGame() {
+  this.score = 10;
+  this.level = 0;
+  this.$grid.removeClass().addClass(this.arrayOfRounds[0]);
+  this.i = 0;
+  rpdg.clearingClassesAndDivs();
+};
+
+rpdg.playResetMusic = function playResetMusic() {
+  this.$resultAudio.src = ('/public/assets/audio/reset.mp3');
+  this.$resultAudio.play();
+};
+
+rpdg.getBonusInput = function getBonusInput() {
+
+  event.preventDefault();
+  const $bonusAnswerQueenLowerCase = this.$bonusAnswerQueen.val().toLowerCase();
+  const $bonusAnswerSeasonNumber = this.$bonusAnswerSeason.val();
+
+  if (($bonusAnswerQueenLowerCase === 'adore delano') && ($bonusAnswerSeasonNumber === '6')) {
+    this.score += 10;
   }
 
-  function clearingClassesAndDivs() {
-    $takeTile.show();
-    $tiles.removeClass('hidden');
-    $result.html('');
-    $gifs.attr('src', '/public/assets/images/blank.gif');
-    arrayOfTiles = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-    $playerScore.html(score);
-  }
+  this.$bonusRound.removeClass('roundAppear');
+  this.$complete.addClass('appear');
+  this.$completeAudio.src = ('/public/assets/audio/peanut-butter.mp3');
+  this.$completeAudio.play();
+  this.$finalScore.html(this.score);
+};
 
-  function nextRound() {
-    $submitBtn.show();
-    score += 10;// add ten for the next level
-    i++;
-    $grid.addClass(arrayOfRounds[level]);
-    $nextRoundBtn.hide();//hide the next round button
-    clearingClassesAndDivs();
-  }
+rpdg.replayGame = function replayGame() {
+  rpdg.resetGame();
+  this.$complete.removeClass('appear');
+  this.$bonusRound.hide();
+  this.$bonusRound.removeClass('roundAppear');
+  this.$body.removeClass('frozen');
+  this.$completeAudio.src = ('');
+};
 
-  function resetGame() {
-    score = 10;
-    level = 0;
-    console.log($grid.removeClass().addClass(arrayOfRounds[0]));
-    i = 0;
-    clearingClassesAndDivs();
-  }
+rpdg.setup = function setup() {
+  this.$introAudio = $('.introButtonAudio')[0];
+  this.$resultAudio = $('.resultAudio')[0];
+  this.$takeTileAudio = $('.takeTileAudio')[0];
+  this.$completeAudio = $('.completeAudio')[0];
+  this.$takeTile = $('.takeTile');
+  this.$tiles = $('.tiles');
+  this.$playerScore = $('.playerScore');
+  this.$result = $('.result');
+  this.$gifs = $('#gifs');
+  this.$reset = $('.reset');
+  this.$introBtn = $('#introBtn');
+  this.$grid = $('#grid');
+  this.$submitBtn = $('.submitAnswer');
+  this.$complete = $('.complete');
+  this.$bonusRound = $('.bonusRound');
+  this.$body = $('body');
+  this.$afterRoundsScore = $('.afterRoundsScore');
+  this.$nextRoundBtn = $('.nextRoundBtn');
+  this.$playerAnswer = $('#playerAnswer');
+  this.$form = $('form');
 
-  function playResetMusic() {
-    $resultAudio.src = ('/public/assets/audio/reset.mp3');
-    $resultAudio.play();
-  }
+  this.$bonusAnswerQueen = $('#nameQueen');
+  this.$bonusAnswerSeason = $('#numberSeason');
+  this.$submitBonusAnswer = $('.submitBonusAnswer');
+  this.$finalScore = $('.finalScore');
+  this.$show = $('.show');
+  this.$logo = $('.logo');
+  this.$footer = $('footer');
+  this.$main = $('main');
+  this.$replay = $('.replay');
 
-  function getBonusInput() {
+  // $logo.hide();
+  // $footer.hide();
 
-    event.preventDefault();
-    const $bonusAnswerQueenLowerCase = $bonusAnswerQueen.val().toLowerCase();
-    const $bonusAnswerSeasonNumber = $bonusAnswerSeason.val();
+  this.$playerScore.html(this.score);
+  this.$grid.addClass(this.arrayOfRounds[this.level]);
+  this.$nextRoundBtn.hide();
 
-    if (($bonusAnswerQueenLowerCase === 'adore delano') && ($bonusAnswerSeasonNumber === '6')) {
-      score += 10;
-    }
+  this.$introBtn.on('click', this.hideIntroduction.bind(this));
+  this.$introBtn.on('click', this.soundClipIntro.bind(this));
 
-    $bonusRound.removeClass('roundAppear');
-    $complete.addClass('appear');
-    $completeAudio.src = ('/public/assets/audio/peanut-butter.mp3');
-    $completeAudio.play();
-    $finalScore.html(score);
-  }
+  this.$takeTile.on('click', this.takeTile.bind(this));
+  this.$takeTile.on('click', this.updateScore.bind(this));
 
-  function replayGame() {
-    resetGame();
-    $complete.removeClass('appear');
-    $bonusRound.hide();
-    $bonusRound.removeClass('roundAppear');
-    $body.removeClass('frozen');
-    $completeAudio.src('');
-  }
+  this.$form.on('submit', this.getInput.bind(this));
+  this.$form.on('submit', this.clearInput.bind(this));
 
+  this.$nextRoundBtn.on('click',this.nextRound.bind(this));
 
-  $('#introBtn').on('click', hideIntroduction);
-  $('#introBtn').on('click', soundClipIntro);
-
-  $takeTile.on('click', takeTile);
-  $takeTile.on('click', updateScore);
-
-  $form.on('submit', getInput);
-  $form.on('submit', clearInput);
-
-  $nextRoundBtn.on('click', nextRound);
-
-  $reset.on('click', resetGame);
-  $reset.on('click', playResetMusic);
+  this.$reset.on('click', this.resetGame.bind(this));
+  this.$reset.on('click', this.playResetMusic.bind(this));
 
 
-  $submitBonusAnswer.on('click', getBonusInput);
+  this.$submitBonusAnswer.on('click', this.getBonusInput.bind(this));
 
-  $('.replay').on('click', replayGame);
-
-
+  this.$replay.on('click', this.replayGame.bind(this));
 
 
+};
 
 
-
-
-
-
-
-
-
-
-
-});
+$(rpdg.setup.bind(rpdg));
